f3fed5c5967ee30b21c45f980c5865d8b1d943bd
Raid <r4ideddesigns@gmail.com>
2016-11-17 11:18:36 +1000

    Init

---
+++ TechSupportApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TechSupportApp.Global" Language="C#" %>
---
+++ TechSupportApp/Prog_Class/Technician.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechSupportApp.Prog_Class
+{
+    public class Technician
+    {
+        private int techID { get; }
+        private string name { get; set; }
+        private string email { get; set; }
+        private int phone { get; set; }
+        private string password { get; }
+        private int level { get; }
+
+        public Technician()
+        {
+
+        }
+    }
+}
