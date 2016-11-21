06aa8c932b5727948a9454c0e7a4a20c5bfa94f1
BigRhysy <rhys-cunningham@hotmail.com>
2016-11-21 14:52:57 +1000

    ViewCust working
    Need to add product registration for customers still.

---
+++ Site/Customer/ViewCust.aspx.cs
@@ -0,0 +1,62 @@
+﻿//////
+/// Created by: Rhys Cunningham, 4101651512
+/// Date modified: 18-11-2016
+/// Purpose: Add new Customers
+/// Known bugs: 
+//////
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TechSupportApp
+{
+    public partial class _ViewCust : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string _CustomerID = Convert.ToString(Session["CustomerID"]);
+                using (SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Rhys\Documents\GitHub\TechSupport\TechSupportApp\App_Data\TechSupport.mdf;Integrated Security=True;Connect Timeout=30"))
+                {
+                    SqlCommand selected = new SqlCommand("SELECT * FROM Customers WHERE CustomerID = @_CustomerID");
+                    selected.Parameters.Add("_CustomerID", SqlDbType.VarChar).Value = _CustomerID;
+                    selected.Connection = conn;
+                    conn.Open();
+
+                    SqlDataReader reader = selected.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        tbName.Text = (reader["Name"].ToString());
+                        tbAddress.Text = (reader["Address"].ToString());
+                        tbCity.Text = (reader["City"].ToString());
+                        tbState.Text = (reader["State"].ToString());
+                        tbZip.Text = (reader["ZipCode"].ToString());
+                        tbEmail.Text = (reader["Email"].ToString());
+                        tbPhone.Text = (reader["Phone"].ToString());
+
+                    }
+
+                    conn.Close();
+                }
+            } catch (Exception ex)
+            {
+                ex.Message.ToString(); 
+            }
+            
+           
+            
+        }
+
+        protected void regCustSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Site/Ticket/AddIncident.aspx.cs
@@ -0,0 +1,39 @@
+﻿//////
+/// Created by: Bourne Gordon-Krueger, 0103285514
+/// Date modified: 17-11-2016
+/// Purpose: Add new Technicians
+/// Known bugs: 
+//////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TechSupportApp
+{
+    public partial class _AddIncident : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void newTechSubmit_Click(object sender, EventArgs e)
+        {
+            SqlDataSourceAddTech.InsertCommandType = SqlDataSourceCommandType.Text;
+            SqlDataSourceAddTech.InsertCommand = "INSERT INTO [Technicians] ([Name], [Email], [Phone], [Level], [Password]) VALUES (@Name, @Email, @Phone, @Level, @Password)";
+
+            SqlDataSourceAddTech.InsertParameters.Add("Name", tbName.Text);
+            SqlDataSourceAddTech.InsertParameters.Add("Email", tbEmail.Text);
+            SqlDataSourceAddTech.InsertParameters.Add("Phone", tbPhone.Text);
+            SqlDataSourceAddTech.InsertParameters.Add("Level", tbLevel.Text);
+            SqlDataSourceAddTech.InsertParameters.Add("Password", tbPassword.Text);
+            SqlDataSourceAddTech.Insert();
+
+            Server.Transfer("~/Site/Tech/UpdateTech.aspx", false);
+        }
+    }
+}
